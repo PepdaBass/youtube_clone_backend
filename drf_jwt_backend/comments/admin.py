7a8de80473c95153f0484d8e8cc98c3768b2cc13
Jeffrey Peplinski <jeffreypeplinski@jeffreys-mbp.lan>
2022-02-22 16:03:00 -0500

    Added Reply model and update Comment model.

--- drf_jwt_backend/comments/admin.py
+++ drf_jwt_backend/comments/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Comments
+from .models import Comment
+from .models import Reply
 
 # Register your models here.
-admin.site.register(Comments)
+admin.site.register(Comment)
+admin.site.register(Reply)
 
