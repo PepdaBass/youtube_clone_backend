76972eaf1d26abf86ae21dddbefa71c76f1feb3b
ckatjrdavis <ckatjrdavis@gmail.com>
2022-03-02 15:57:22 -0600

    added custom UserSerializer to comment and reply serializers.

--- drf_jwt_backend/comments/serializers.py
+++ drf_jwt_backend/comments/serializers.py
@@ -1,13 +1,22 @@
 from rest_framework import serializers
 from .models import Comment, Reply
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ['id', 'username']
 
 class CommentSerializer(serializers.ModelSerializer):
+  user = UserSerializer(many=False, read_only=True)
   class Meta:
     model = Comment
-    fields = ['id','video_id', 'text', 'likes', 'dislikes', 'user_id']
+    fields = ['id','video_id', 'text', 'likes', 'dislikes', 'user_id', 'user']
 
 
 class ReplySerializer(serializers.ModelSerializer):
+  user = UserSerializer(many=False, read_only=True)
   class Meta:
     model = Reply
     fields = ['id', 'text', 'comment', 'user_id']
+
